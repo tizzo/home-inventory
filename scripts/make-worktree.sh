f1bcee748886d95265346e345686970475211e35
Howard Tyson <howard@howardtyson.com>
2026-01-25 19:26:03 -0500

    Adding scripts for managing local worktrees.

--- scripts/make-worktree.sh
+++ scripts/make-worktree.sh
@@ -13,3 +13,11 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 WORKTREE_DIR="$REPO_ROOT/.worktrees"
 git worktree add -b "$WORKTREE_NAME" "$WORKTREE_DIR/$WORKTREE_NAME" main
 cp "${REPO_ROOT}/backend/.env" "$WORKTREE_DIR/$WORKTREE_NAME/backend/.env"
+cp -r "${REPO_ROOT}/.claude" "$WORKTREE_DIR/$WORKTREE_NAME/.claude"
+echo "Worktree '$WORKTREE_NAME' created at '$WORKTREE_DIR/$WORKTREE_NAME'"
+echo "run to enter worktree:
+``````````
+
+    cd $WORKTREE_DIR/$WORKTREE_NAME
+``````````
+"
---
+++ scripts/rm-worktree.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+cd $(dirname "$0")
+set -e
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <worktree-name>"
+    exit 1
+fi
+# Make sure the worktree exists
+if ! git worktree list | grep -q "$1"; then
+    echo "Worktree '$1' does not exist."
+    exit 1
+fi
+# Make sure there isn't anything uncommitted in the worktree
+if [ -n "$(git -C ".worktrees/$1" status --porcelain)" ]; then
+    echo "Worktree '$1' has uncommitted changes. Please commit or stash them before removing the worktree."
+    exit 1
+fi
+
+WORKTREE_NAME="$1"
+REPO_ROOT=$(git rev-parse --show-toplevel)
+WORKTREE_DIR="$REPO_ROOT/.worktrees"
+git worktree remove "$WORKTREE_DIR/$WORKTREE_NAME"
+rm -rf "$WORKTREE_DIR/$WORKTREE_NAME"
+echo "Worktree '$WORKTREE_NAME' has been removed."
+
